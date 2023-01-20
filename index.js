e1d29ac21d8bdcc9d9d24508065b4a3f8c75bb47
Satyajit Nayak <77431114+PuL5TaR@users.noreply.github.com>
2023-01-20 21:39:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const cstm_pointer = document.querySelector('.cstm-pointer');
+const cstm_span = document.querySelector('.cstm-span');
+const span = cstm_span.querySelector('span');
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+let clickCount = 0;
+
+document.addEventListener('mousemove', async (e) => {
+    await sleep(200);
+    // console.log(e.pageX);
+    cstm_pointer.style.opacity = "10";
+    cstm_pointer.style.left = `${e.pageX}px`;
+    cstm_pointer.style.top = `${e.pageY}px`;
+});
+
+
+
+document.addEventListener('click', () => {
+    if (clickCount == 0) {
+        cstm_span.style.color = '#F273E6';
+        span.innerHTML = 'The world is a beatiful place';
+    } else if (clickCount == 1)
+        span.innerHTML = 'The world is a beatiful place<br>with U in It';
+    else span.innerHTML = 'Happy Life<br>✌<br>[ refresh ]';
+    clickCount++;
+});
